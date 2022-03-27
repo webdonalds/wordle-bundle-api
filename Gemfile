8993840e24117e9c6d2b03ff3ed302f1736d48e9
Hoerin Doh <hellodhlyn@gmail.com>
2022-03-27 23:18:09 +0900

    Hello, world!

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source "https://rubygems.org"
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+ruby "3.1.1"
+
+# Rails
+gem "rails", "~> 7.0.2", ">= 7.0.2.3"
+
+# Drivers
+gem "pg", "~> 1.1"
+
+# Web server
+gem "puma", "~> 5.0"
+
+# Ruby extensions
+gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
+
+group :development, :test do
+  gem "debug", platforms: %i[ mri mingw x64_mingw ]
+end
+
+group :development do
+end
---
+++ config/application.rb
@@ -0,0 +1,19 @@
+require_relative "boot"
+
+require "rails"
+require "active_model/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module WordleBundleApi
+  class Application < Rails::Application
+    config.load_defaults 7.0
+    config.api_only = true
+  end
+end
